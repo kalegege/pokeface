57507faad392dca987f148ad7ac3e72eeac8e87d
kalegege <xv88133536@163.com>
2016-09-30 17:16:53 +0800

    refresh

--- pokeface/src/main/java/com/wasu/pokeface/Controller/WebController.java
+++ pokeface/src/main/java/com/wasu/pokeface/Controller/WebController.java
@@ -1,9 +1,14 @@
 package com.wasu.pokeface.Controller;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 public class WebController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PropertyConfigurator.configure(ClassLoader.getSystemResource("log4j.properties"));
+		Logger log= Logger.getLogger(WebController.class);
+		log.info("大功告成，我的log写进my.log文件了！");
 		System.out.println("two");
 	}
 
