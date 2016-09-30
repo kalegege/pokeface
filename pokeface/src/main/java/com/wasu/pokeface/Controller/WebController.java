777aaa38e167d5296cfd45f45f0a4a8877d324ab
kalegege <xv88133536@163.com>
2016-09-30 13:58:01 +0800

    webcontroller

---
+++ pokeface/src/main/java/com/wasu/pokeface/Controller/WebController.java
@@ -0,0 +1,10 @@
+package com.wasu.pokeface.Controller;
+
+public class WebController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("two");
+	}
+
+}
