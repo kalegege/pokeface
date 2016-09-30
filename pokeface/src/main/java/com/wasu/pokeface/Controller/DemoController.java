b7c2a8c1b9760eea2fe7b53f230d8d2a0238d264
kalegege <xv88133536@163.com>
2016-09-30 13:56:31 +0800

    next item

--- pokeface/src/main/java/com/wasu/pokeface/Controller/DemoController.java
+++ pokeface/src/main/java/com/wasu/pokeface/Controller/DemoController.java
@@ -5,6 +5,7 @@ public class DemoController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first demo!");
+			System.out.println("next item!");
 	}
 
 }
